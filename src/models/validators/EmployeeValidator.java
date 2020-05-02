4b74a442bbaeeb734fe3070cdd3a2c14d6e67de7
amurin <tatsuya061821991@gmail.com>
2020-05-03 00:13:55 +0900

    Add Employee class

---
+++ src/models/validators/EmployeeValidator.java
@@ -0,0 +1,12 @@
+package models.validators;
+
+import java.util.List;
+
+import models.Employee;
+
+public class EmployeeValidator {
+    public static List<String> validate(Employee e, Boolean code_duplicate_check_flag, Boolean password_check_flag)
+    {
+
+    }
+}
